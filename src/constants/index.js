3e4ad08af047a2993202d31b09d0d0d2233407a0
Viktoria <viktorialuhverchik@gmail.com>
2020-07-27 00:40:17 +0300

    9

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const api = 'http://localhost:3000/api';
-// const api = 'https://fanfics-backend.herokuapp.com/api';
+// const api = 'http://localhost:3000/api';
+const api = 'https://fanfics-backend.herokuapp.com/api';
 
 export default api;
